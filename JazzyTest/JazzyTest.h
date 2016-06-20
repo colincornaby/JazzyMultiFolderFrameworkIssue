04b8d67d308c24e7e0db65ba89dc472830d63caa
Colin Cornaby <colin.cornaby@digimarc.com>
2016-06-20 11:46:28 -0700

    Initial Commit

---
+++ JazzyTest/JazzyTest.h
@@ -0,0 +1,19 @@
+//
+//  JazzyTest.h
+//  JazzyTest
+//
+//  Created by Cornaby, Colin on 6/20/16.
+//  Copyright © 2016 Digimarc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JazzyTest.
+FOUNDATION_EXPORT double JazzyTestVersionNumber;
+
+//! Project version string for JazzyTest.
+FOUNDATION_EXPORT const unsigned char JazzyTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JazzyTest/PublicHeader.h>
+
+
